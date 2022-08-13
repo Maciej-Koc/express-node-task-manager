a5f31c288790498405873582b3a53d3b4dfc93ef
Maciej-Koc <mkoc2@asu.edu>
2022-08-13 16:46:10 -0500

    Initial commit with partially completed routes and task file

---
+++ starter/controllers/tasks.js
@@ -0,0 +1,29 @@
+function getAllTasks(req, res) {
+  res.send("get all tasks");
+}
+
+function createTask(req, res) {
+  res.json(req.body);
+}
+
+//test
+
+function getTask(req, res) {
+  res.json({ id: req.params.id });
+}
+
+function updateTask(req, res) {
+  res.send("update task");
+}
+
+function deleteTask(req, res) {
+  res.send("delete task");
+}
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
